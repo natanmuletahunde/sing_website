87c935cc5ba51dbf27d9aad64dc08a425db1450c
pivnati <natanmuleta911@gmail.com>
2024-09-27 12:21:37 -0700

    ccreate the listofmusic

---
+++ src/pages/ListOfMusic.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+// src/pages/ListOfMusic.jsx
+import React from 'react';
+
+const ListOfMusic = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-3xl font-bold">List of Music</h2>
+      {/* Add music list here */}
+    </div>
+  );
+};
+
+export default ListOfMusic;
