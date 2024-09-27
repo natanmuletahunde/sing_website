cfd1f1bbde740c6ec2a3fed6a8bc564f6421475a
pivnati <natanmuleta911@gmail.com>
2024-09-27 12:22:21 -0700

    ccreate the aboutus.jsx

---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+// src/pages/AboutUs.jsx
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-3xl font-bold">About Us</h2>
+      <p>This is a music website that showcases the latest songs and artists.</p>
+    </div>
+  );
+};
+
+export default AboutUs;
