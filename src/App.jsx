42ee68e6df790960da218422c5254e74a3135a1f
pivnati <natanmuleta911@gmail.com>
2024-09-27 12:23:44 -0700

    modify the app.jsx including all that components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
+/* eslint-disable no-unused-vars */
+// src/App.jsx
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import Player from './pages/Player';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
+import ListOfMusic from './pages/ListOfMusic';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div className="App bg-gray-900 text-white">
-        <Navbar />
+      <Header />
+      <main className="min-h-screen">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<Search />} />
-          <Route path="/player/:id" element={<Player />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/list" element={<ListOfMusic />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </div>
+      </main>
+      <Footer />
     </Router>
   );
-}
+};
 
 export default App;
